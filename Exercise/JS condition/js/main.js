3c6021c53a0140322f8d3a8426a81578466c5ef6
Morning <morning@mstars-lab2-02s-iMac.local>
2022-12-13 13:43:36 +0800

    Update from school

---
+++ Exercise/JS condition/js/main.js
@@ -0,0 +1,95 @@
+// let a = 85;
+// let b = 75;
+// let c = 96;
+// let d = 69;
+
+// if (a >= 80){
+//     console.log(a);
+// } else {
+//     a = 0;
+// }
+// if (b >= 80){
+//     console.log(b);
+// } else {
+//     b = 0;
+// }
+// if (c >= 80){
+//     console.log(c);
+// } else {
+//     c = 0;
+// }
+// if (d >= 80){
+//     console.log(d);
+// } else {
+//     d = 0;
+// }
+// console.log( a + b + c + d);
+
+// let a = 3;
+// let b = 7;
+// let c = 2;
+// let d = 4;
+
+// if (a <= 5){
+//     console.log(a);
+// } else {
+//     a = 1;
+// }
+
+// if (b <= 5){
+//     console.log(b);
+// } else {
+//     b = 1;
+// }
+
+// if (c <= 5){
+//     console.log(c);
+// } else {
+//     c = 1;
+// }
+
+// if (d <= 5){
+//     console.log(d);
+// } else {
+//     d = 1;
+// }
+// console.log(a*b*c*d);
+
+// let i = 4 > 3; //true
+// let iii = 4 < 3; //false
+// let ii = 4 >= 3; //true
+// let iv = 4 <= 3; //false
+// let v = 4 == 4; //true
+// let vi = 4 === 4; //true
+// let vii = 4 != 4; //false
+// let iix = 4 !== 4; //false
+// let ix = 4 != "4"; //false
+// let x = 4 == "4"; //true
+// let xi = 4 === "4"; //false
+
+// console.log(xi)
+
+// let a = Number(prompt("give me your age"));
+// let b = Number(prompt("give me your age"));
+
+// if ( a < b ) {
+//     console.log("Чи хөгшин пзда");
+// } else {
+//     console.log("Би ах сда");
+// }
+
+// let days = Number(prompt("Give me your day"));
+
+// if ( 1 <= days && days <= 5){
+//     console.log("It's Weekday");
+// } else if ( 6 <= days && days <= 7) {
+//     console.log("It's Weekend");
+// } else {
+//     console.log("Please put week days number")
+// }
+
+// let hours = Number(prompt("Please insert your worked hours"));
+// let ratePerHour = 28;
+
+// console.log(hours * ratePerHour + "$");
+
